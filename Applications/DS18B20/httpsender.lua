ddfb1e1cd74ffeb7764d70373c7a0574ba5ce8d2
ivan.orfanidi <fhfh@mail.ru>
2016-09-01 15:35:29 +0300

    Добавил вывод мак адреса esp.

--- Applications/DS18B20/httpsender.lua
+++ Applications/DS18B20/httpsender.lua
@@ -75,17 +75,15 @@ conn:on("receive", function(conn, payloadout)
         end
     end)
 -- api.thingspeak.com 184.106.153.149
---conn:connect(80,'184.106.153.149')
-conn:connect(8283,'narodmon.ru')
+conn:connect(80,'184.106.153.149')
 
 conn:on("connection", function(conn)
     ow.setup(OWPIN)
-    --t=getData()
-    t=25
+    t=getData()
     print("Temp:"..t.." C\n")
     print("Send data...")
-    print("#"..wifi.sta.getmac().."#ESP8266\n#T1#"..t.."\n##")
-    conn:send("#"..wifi.sta.getmac().."#ESP8266\n#T1#"..t.."\n##")
+    print("GET /update?key=AQBLFGNJKJFJED2P&field1="..t.."\r\n")
+    conn:send("GET /update?key=AQBLFGNJKJFJED2P&field1="..t.."\r\n")
 end)
 
 conn:on("sent",function(conn)
--- Applications/DS18B20/init.lua
+++ Applications/DS18B20/init.lua
@@ -9,8 +9,8 @@ if wifi.sta.getip()== nil then
 print("IP unavaiable, Waiting...") 
 else 
 tmr.stop(1)
-print(wifi.sta.getmac())                            -- print current mac address
-print("Config done, IP is "..wifi.sta.getip())      -- print current IP address
+print("MAC: "..wifi.sta.getmac())      -- print current mac address
+print("IP: "..wifi.sta.getip())      -- print current IP address
 dofile("httpsender.lua")
 end 
 end)
