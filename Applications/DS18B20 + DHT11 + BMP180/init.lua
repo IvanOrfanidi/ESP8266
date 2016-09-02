0d9d832b9460b9cfbdef058dc56fd1a193c2bb72
ivan.orfanidi <fhfh@mail.ru>
2016-09-02 17:17:34 +0300

    Соединил проекты по метеодатчикам DS18B20, DHT11 и BMP180.

---
+++ Applications/DS18B20 + DHT11 + BMP180/init.lua
@@ -0,0 +1,16 @@
+--init.lua
+print("Setting up WIFI...")
+wifi.setmode(wifi.STATION)
+--modify according your wireless router settings
+wifi.sta.config("Xiaomi_D36F","xiaomixiaomi")
+wifi.sta.connect()
+tmr.alarm(1, 1000, 1, function() 
+if wifi.sta.getip()== nil then 
+print("IP unavaiable, Waiting...") 
+else 
+tmr.stop(1)
+print("MAC: "..wifi.sta.getmac())      -- print current mac address
+print("IP: "..wifi.sta.getip())      -- print current IP address
+dofile("httpsender.lua")
+end 
+end)
